1db2f21e13903c74ad5dc6546240148a3e2511f0
zhmireva <119106856+zhmireva@users.noreply.github.com>
2023-05-10 13:06:11 +0300

    Create messages.py

---
+++ data/messages.py
@@ -0,0 +1,16 @@
+# Переменные с информационными сообщениями об ошибках
+
+
+wrong_login_or_pass_message = 'Неверный логин или пароль'
+wrong_capcha_message = 'Неверно введен текст с картинки'
+wrong_name_message = 'Необходимо заполнить поле кириллицей. От 2 до 30 символов.'
+wrong_surname_message = 'Необходимо заполнить поле кириллицей. От 2 до 30 символов.'
+wrong_login_message = 'Введите телефон в формате +7ХХХХХХХХХХ или +375XXXXXXXXX, или email в формате example@email.ru'
+wrong_password_message = 'Длина пароля должна быть не менее 8 символов'
+wrong_recovery_message = 'Неверный логин или текст с картинки'
+
+vk_wrong_message = 'Указан неверный логин или пароль.'
+ok_wrong_message = 'Неверный логин или пароль'
+mail_wrong_message = 'Неверное имя пользователя или пароль'
+google_wrong_message = 'Не удалось найти аккаунт Google.'
+yandex_wrong_message = 'Такой логин не подойдет'
