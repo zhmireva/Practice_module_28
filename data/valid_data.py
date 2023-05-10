63a2405a4934bc96a0e384d79752c3765500ec72
zhmireva <119106856+zhmireva@users.noreply.github.com>
2023-05-10 13:04:50 +0300

    Create valid_data.py

---
+++ data/valid_data.py
@@ -0,0 +1,7 @@
+# Переменные с валидными  данными для тестов
+
+correct_logins = ['zhmireva@gmail.com', '79295944220']
+correct_pass = 'Zh123456'
+
+agreement_url = 'https://b2c.passport.rt.ru/sso-static/agreement/agreement.html'
+agreement_title = 'Публичная оферта о заключении Пользовательского соглашения на использование Сервиса «Ростелеком ID»'
