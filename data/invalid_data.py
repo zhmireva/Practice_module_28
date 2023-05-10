52005dc736e9fda6b06cf8c74adfaab590d8388f
zhmireva <119106856+zhmireva@users.noreply.github.com>
2023-05-10 13:04:10 +0300

    Create invalid_data.py

---
+++ data/invalid_data.py
@@ -0,0 +1,12 @@
+# Переменные с НЕвалидными  данными для тестов
+
+incorrect_names = ['а', 'z', 'аб', 'AZ', '12345']
+incorrect_surnames = ['а', 'z', 'аб', 'AZ', '12345']
+incorrect_phones = ['5678542789', '0000000000', '-45', '10.55', '254126358585868686868668']
+incorrect_emails = ['tests@tests.com', 'tests2test@tests.com']
+incorrect_passwords = ['-5_68q', 'роывhk', '@@@@789', '---_=+', 'x']
+incorrect_logins = ['sadsdasdasd', '+71213252111']
+incorrect_phone = '71213252111'
+incorrect_email = 'assdsddas@bademmailrasras.cam'
+incorrect_login_yandex = 'zh34583fdf3@ya.rus'
+incorrect_pass = 'zhsud2312asdas'
